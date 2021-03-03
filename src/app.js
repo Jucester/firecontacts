97a741fd40b67d8eb579aff39f6b3a82c026ad1f
Jucel Dorante <juceld@gmail.com>
2021-03-02 23:20:16 -0400

    First commit

---
+++ src/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const morgan = require('morgan');
+const hbs = require('express-handlebars');
+const path = require('path');
+
+const app = express();
+
+// Settings
+app.set('PORT', process.env.PORT || 4000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', hbs({
+    defaultLayout: 'main',
+    extname: '.hbs',
+
+}));
+app.set('view engine', '.hbs');
+
+// Middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Routes
+app.use('/', require('./routes/index.routes'));
+
+// Static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+module.exports = app;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+const app = require('./app');
+
+
+app.listen(app.get('PORT'), () => {
+    console.log('Server on port ', app.get('PORT'))
+});
